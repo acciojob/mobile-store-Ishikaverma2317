d69e3e3221c2e2f0f9b3e92ceb563d0030d81e7d
Ishika Verma <225302307+Ishikaverma2317@users.noreply.github.com>
2026-01-05 12:24:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
-
-
+import App from "./App";   // 🔥 components hata diya
 
 ReactDOM.render(<App />, document.getElementById("root"));
